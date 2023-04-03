20ec3f1fa1bc83dad77e9c95bafd8dbb8ca0a9e6
Shamreen <sham.qureshi37@gmail.com>
2023-04-03 12:33:27 +0530

    UI updates

--- frontend/src/components/NavbarInner.js
+++ frontend/src/components/NavbarInner.js
@@ -1,41 +1,33 @@
-import React from 'react'
+import React, { Profiler } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../img/logo.png'
+import profile from '../img/profile-img.png'
 const NavbarInner = () => {
   return (
     <>
-    <nav className="navbar position-sticky box-shadow py-3 navbar-expand-lg navbar-light">
+    <nav className="navbar position-sticky box-shadow rounded-0 py-2 navbar-expand-lg navbar-light">
   <div className="container">
-    <img className='navbar-brand' src={logo}/>
+    <img className='navbar-brand inner-nav-logo' src={logo}/>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto mx-2 mb-2 mb-lg-0">
+      <ul className="navbar-nav m-auto  mb-2 mb-lg-0">
         <li className="nav-item">
           <Link to="/home" className='nav-link'> Home</Link>
         </li>
         <li className="nav-item mx-2">
-        <Link to="/blogs" className='nav-link'> Blogs</Link>
+        <Link to="/blogs" className='nav-link'> Jobs</Link>
         </li>
-        <li className="nav-item dropdown mx-2">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"/></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li className="nav-item mx-2">
+        <Link to="/blogs" className='nav-link'> Blogs</Link>
         </li>
        
       </ul>
-      <form className="d-flex">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-primary" type="submit">Search</button>
-      </form>
+      
     </div>
+   <div> <i class="fa-sharp fa-solid fa-bell nav-icon me-3"></i> </div>
+  <div> <img className='nav-profile' src={profile}/></div>
   </div>
 </nav>
     </>
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import '../css/home.css'
-import { Link } from 'react-router-dom'
 import NavbarInner from '../components/NavbarInner'
 import google from '../img/google.png'
 import profile from '../img/profile-img.png'
@@ -9,10 +8,10 @@ const Home = () => {
     <>
       <NavbarInner />
       <div className="home-section">
-        <div className="container-fluid py-4">
+        <div className="container-fluid p-3">
           <div className="row">
             {/* ======First column============ */}
-            <div className="col-md-3 homecol-1">
+            <div className="col-md-3 homecol-1 p-2">
               <div className="card text-center py-3 box-shadow border-0">
                 <h5>Complete your profile <i class="fa-regular fa-pen-to-square"></i> </h5>
                 <hr />
@@ -25,28 +24,56 @@ const Home = () => {
                   <a href="#" className="btn btn-primary">View Profile</a>
                 </div>
               </div>
+
               <div className="card mt-3 box-shadow border-0">
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                  <h5 className="card-title mb-3">Work Experience</h5>
+
+                  <div className="latest-jobs-home d-flex w-100 justify-content-between">
+                  <div className='latest-job-home-logo-div d-flex flex-column justify-content-around'>
+                  <img src={google} alt="" className='home-work-exp-logo' />
+                 </div>
+                  <div className="latest-jobs-home-inner-div">
+                    <h6 className='mb-0'>Full Stack Developer Intern</h6>
+                    <p>Company Name <span> &#8226;2021-2022</span> </p>       
+                  </div>
+                </div>
+                  
+                <div className="latest-jobs-home d-flex w-100 justify-content-between">
+                  <div className='latest-job-home-logo-div d-flex flex-column justify-content-around'>
+                  <img src={google} alt="" className='home-work-exp-logo' />
+                 </div>
+                  <div className="latest-jobs-home-inner-div">
+                    <h6 className='mb-0'>Full Stack Developer Intern</h6>
+                    <p>Company Name</p>       
+                  </div>
+                </div>
+
                 </div>
               </div>
+
               <div className="card mt-3 box-shadow border-0">
                 <div className="card-body">
-                  <h5 className="card-title">Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
+                  <h5 className="card-title">Skills and Expertise</h5>
+                  
+                  <p className='skill-name'>HTML</p> 
+                  <p className='skill-name'>CSS</p>
+                  <p className='skill-name'>JavaScript</p>
+                  <p className='skill-name'>Cloud Computing</p>
+                  <p className='skill-name'>Machine Learning</p>
+                    
+                    
                 </div>
               </div>
             </div>
             {/* ======Second column============ */}
-            <div className="col-md-6 homecol-2">
+            <div className="col-md-6 homecol-2 p-2">
               <div className="search-home-div box-shadow p-3">
-                <h3>Start your job search</h3>
+                <h4>Are you looking for a dream job?</h4>
+                <p className='text-light'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate iusto odio itaque a optio reiciendis.</p>
                 <form className="d-flex">
-                  <input className="form-control me-2" type="search" placeholder="Search Keyword" aria-label="Search" />
-                  <button className="btn btn-primary" type="submit">Search</button>
+                  <input className="form-control search-home-input me-2" type="search" placeholder="Search your dream job here" aria-label="Search" />
+                  <button className="btn btn-light search-btn-home" type="submit">Search</button>
                 </form>
               </div>
 
@@ -54,19 +81,48 @@ const Home = () => {
                 <div className="latest-jobs-home d-flex w-100 justify-content-between">
                   <div className="latest-jobs-home-inner-div">
                     <h5>Full Stack Developer Intern</h5>
-                    <p>Company Name</p>
-                    <p>Experience: 0-5 Years</p>
-                    <p>Salary: 5,00,000-7,00,000</p>
-                    <p>Location</p>
+                    <p>Company Name  </p>
+                    <p> <span> &#8226;Remote work</span>  <span> &#8226; &#x20B9;5,00,000-7,00,000 </span> <span> &#8226; 0-5 years experience</span>   </p>
+                    
                   </div>
                  <div className='latest-job-home-logo-div d-flex flex-column justify-content-around'>
                   <img src={google} alt="" className='home-latest-job-updates-logo d-block' />
                  <button className="btn btn-primary apply-now-home-btn"> Apply Now </button>
                  </div>
-
                 </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus recusandae dignissimos placeat minus excepturi amet. Deleniti, placeat. At eaque minima omnis voluptate numquam aperiam quae?</p>
                 <hr />
                 
+                <div className="latest-jobs-home d-flex w-100 justify-content-between">
+                  <div className="latest-jobs-home-inner-div">
+                    <h5>Full Stack Developer Intern</h5>
+                    <p>Company Name  </p>
+                    <p> <span> &#8226;Remote work</span>  <span> &#8226; &#x20B9;5,00,000-7,00,000 </span> <span> &#8226; 0-5 years experience</span>   </p>
+                    
+                  </div>
+                 <div className='latest-job-home-logo-div d-flex flex-column justify-content-around'>
+                  <img src={google} alt="" className='home-latest-job-updates-logo d-block' />
+                 <button className="btn btn-primary apply-now-home-btn"> Apply Now </button>
+                 </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus recusandae dignissimos placeat minus excepturi amet. Deleniti, placeat. At eaque minima omnis voluptate numquam aperiam quae?</p>
+                <hr />
+
+                <div className="latest-jobs-home d-flex w-100 justify-content-between">
+                  <div className="latest-jobs-home-inner-div">
+                    <h5>Full Stack Developer Intern</h5>
+                    <p>Company Name  </p>
+                    <p> <span> &#8226;Remote work</span>  <span> &#8226; &#x20B9;5,00,000-7,00,000 </span> <span> &#8226; 0-5 years experience</span>   </p>
+                    
+                  </div>
+                 <div className='latest-job-home-logo-div d-flex flex-column justify-content-around'>
+                  <img src={google} alt="" className='home-latest-job-updates-logo d-block' />
+                 <button className="btn btn-primary apply-now-home-btn"> Apply Now </button>
+                 </div>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus recusandae dignissimos placeat minus excepturi amet. Deleniti, placeat. At eaque minima omnis voluptate numquam aperiam quae?</p>
+                <hr />
+
                 <div className="latest-jobs-home d-flex w-100 justify-content-between">
                   <div className="latest-jobs-home-inner-div">
                     <h5>Full Stack Developer Intern</h5>
@@ -101,13 +157,39 @@ const Home = () => {
 
 
               </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laborum doloremque amet ullam facilis suscipit iure odio dignissimos, odit nulla tempora quo, repellendus fugiat quae numquam corporis minima perferendis earum. Et minus totam corrupti eos nulla qui, aliquid officiis accusantium atque, perspiciatis laudantium earum quaerat sit maiores ipsam vitae amet nobis. Earum repellat dicta animi at inventore pariatur in velit culpa id saepe, dolore, reiciendis voluptas. Consequatur molestiae possimus, suscipit eveniet quas  consequatur, facilis obcaecati asperiores quo consectetur velit provident quos id architecto perferendis aliquam hic! Veritatis dolorum nisi necessitatibus unde fugit quisquam veniam modi autem nemo, voluptatem quam, dolor sunt illum. Quae necessitatibus eaque blanditiis recusandae velit quis illum error incidunt rem, consectetur ex ipsum. Perspiciatis ducimus praesentium cum quidem omnis. Provident labore ratione obcaecati cum velit laboriosam reiciendis cupiditate tempore dolorum quos quae, amet laborum voluptatibus voluptates quod reprehenderit non molestias dolor totam voluptate autem nostrum magnam! Tenetur omnis vero esse. A saepe praesentium tenetur reprehenderit autem porro pariatur possimus odit similique aperiam tempora ut voluptatem, voluptatum voluptates quos atque neque cum quia, itaque nam! Modi dignissimos, optio dolores, blanditiis tenetur, iste veniam ipsam tempore cumque totam omnis nisi quisquam cupiditate et voluptates labore obcaecati quasi rerum! Asperiores.</p>
+              
             </div>
 
             {/* ======Third column============ */}
 
-            <div className="col-md-3 homecol-3 box-shadow p-3">
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut atque perspiciatis explicabo. Officia, culpa! Maiores officia beatae vitae, recusandae laboriosam perspiciatis illum impedit assumenda culpa soluta repudiandae quod iure voluptate tempora quae enim sit eius modi blanditiis odio distinctio suscipit nesciunt esse est. Voluptas, aut. Expedita aliquam nulla blanditiis consequatur Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio odit fugiat, doloribus veritatis vitae voluptatum nisi soluta. Accusamus porro facilis debitis nulla, distinctio doloremque quidem ipsa autem exercitationem sed odio culpa itaque voluptatibus deserunt iste magnam earum! Eveniet assumenda reprehenderit repellendus corrupti excepturi. Totam voluptates error officiis ea nesciunt praesentium quidem ducimus ipsum culpa molestias qui quos hic fuga, illum quia ullam commodi reiciendis ex iusto tempora iste ut nobis! Aspernatur, quaerat perspiciatis dignissimos minus aperiam natus laudantium esse quasi. Similique aspernatur repellat a nam exercitationem voluptates odit ipsum tempora sequi, distinctio quae vel quidem, quam, in ratione illo dolorem? </p>
+            <div className="col-md-3 homecol-3 p-2">
+            <div className="card box-shadow border-0">
+                <div className="card-body">
+                  <h5 className="card-title mb-3">Latest News </h5>
+
+                  <div className="latest-jobs-home d-flex w-100 justify-content-between">
+                  <div className='latest-job-home-logo-div d-flex flex-column justify-content-around'>
+                  <img src={google} alt="" className='home-work-exp-logo' />
+                 </div>
+                  <div className="latest-jobs-home-inner-div">
+                    <h6 className='mb-0'>Full Stack Developer Intern</h6>
+                    <p>Company Name <span> &#8226;2021-2022</span> </p>       
+                  </div>
+                </div>
+                  
+                <div className="latest-jobs-home d-flex w-100 justify-content-between">
+                  <div className='latest-job-home-logo-div d-flex flex-column justify-content-around'>
+                  <img src={google} alt="" className='home-work-exp-logo' />
+                 </div>
+                  <div className="latest-jobs-home-inner-div">
+                    <h6 className='mb-0'>Full Stack Developer Intern</h6>
+                    <p>Company Name</p>       
+                  </div>
+                </div>
+
+                </div>
+              </div>
+
             </div>
           </div>
         </div>
